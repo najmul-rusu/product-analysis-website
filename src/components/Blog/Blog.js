60f826e344fda56180605847dc9a55c5bff7b8c0
najmul-rusu <nhrusu@gmail.com>
2022-04-05 03:24:08 +0600

    Q & A added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import '../Blog/Blog.css'
 
 const Blog = () => {
     return (
-        <div>
-            This is Blog
+        <div className='qna'>
+            <h2 className='qna-titel text-2xl font-bold'>Question and Answer</h2>
+            <h3 className='text-left text-xl font-bold my-5'>What is Context API?</h3>
+            <p className='text-left'>The React Context API is a way to effectively create global variables for a React app that can be passed around. This is an alternative to "prop drilling" or moving props from grandparents to children to parents. The topic is also referred to as a simple, light method for managing the state using Redox.
+
+                Context API is a (kind of) new feature added to version 16.3 of React that allows one to share (or part of) the whole app lightly and comfortably.</p>
+
+            <h3 className='text-left text-xl font-bold my-5'>What is semantic tag?</h3>
+            <p className='text-left'>The key feature of a semantic element is that it clearly communicates its meaning to both the developer and the browser. These elements clearly define its content.</p>
+            <p className='text-left font-bold my-2'>Examples of semantic tag:</p>
+            <ul className='text-left'>
+                <li>Header tags &lt;h1&gt;&nbsp;through &lt;h6&gt;</li>
+                <li>&lt;blockquote&gt;</li>
+                <li>&lt;code&gt;</li>
+                <li>&lt;em&gt;</li>
+            </ul>
         </div>
     );
 };
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,9 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            404
+            <h1 className='text-center font-extrabold text-red-600 text-9xl mt-20 mb-10'>404</h1>
+            <h3 className='text-center font-bold text-5xl m-10'>That page can’t be found.</h3>
+            <p className='text-center text-xl'>Nothing was found at this location. Try searching, or check out the popular items below.</p>
         </div>
     );
 };
